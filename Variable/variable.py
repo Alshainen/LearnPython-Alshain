e9e3da55c6c3ee8769ea929fa5cf9f679243d7b5
Alshainen <alshainschatten@gmail.com>
2026-09-12 22:21:05 +0700

    Learn Python Day 1 (Variable)

---
+++ Variable/variable.py
@@ -0,0 +1,16 @@
+#Variable = wadah untuk suatu nilai (string, integer, float, boolean)
+
+namaPertama = 'Alshain' #String (teks utuh)
+jumlahUang = 2000 #Integer (angka)
+jumlahUangReceh = 2000.200 #Float (angka desimal)
+siswa = False #Boolean (true)
+hidup = True #Boolean (false)
+
+print(namaPertama)
+#cara manggil variabel
+
+print(f'Halo {namaPertama}')
+#cara manggil variabel dengan teks tambahan juga
+
+
+print(f'Halo {namaPertama}, uangmu Rp{jumlahUang} atau detailnya Rp{jumlahUangReceh}, Kamu siswa: {siswa}, Kamu hidup: {hidup}')
